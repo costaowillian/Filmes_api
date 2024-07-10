5d24a80961ea1d803de3c7f374d68ca208acb9b6
Willian Costa <willian95@outlook.com>
2024-07-10 15:17:56 -0300

    feat: adição de rota post para movies

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,12 @@
 import express ,{ Request, Response } from "express";
+import { createMovie } from "./controllers/MovieControllers";
 
 const router = express.Router();
 
 router.get("/teste", (req:Request, res:Response) => {
     res.status(200).send("API Funcionando!");
 });
+
+router.post("/movie", createMovie);
 
 export default router;
