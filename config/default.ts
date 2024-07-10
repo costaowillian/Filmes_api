53315f771def97503bf581f1dd7fdd5916422f02
Willian Costa <willian95@outlook.com>
2024-07-10 15:17:27 -0300

    fix: configurações das credenciais do mongo db

--- config/default.ts
+++ config/default.ts
@@ -1,7 +1,9 @@
+const dbPassword = process.env.PASSWORD_DB;
+const dbUser = process.env.USERNAME_DB;
+
 export default {
     port: 3030,
-    dbPassword: process.env.PASSWORD_DB,
-    dbUser: process.env.USERNAME_DB,
-    dbUri: `mongodb://localhost:27017/`,
+
+    dbUri: `mongodb://${dbUser}:${dbPassword}@localhost:27017/`,
     env: "development",
 }
