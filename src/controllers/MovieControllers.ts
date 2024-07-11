7a463e412cb2685dba737252d304007064681473
Willian Costa <willian95@outlook.com>
2024-07-11 10:07:30 -0300

    feat: adiçã de metodos para requisições de resgatar, deletar e atualizar entidade movie

--- src/controllers/MovieControllers.ts
+++ src/controllers/MovieControllers.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { MovieModel } from "../models/Movie";
 import Logger from "../../config/logger";
+import { promises } from "dns";
 
 export const createMovie = async (req:Request, res:Response) => {
     try{
@@ -12,3 +13,72 @@ export const createMovie = async (req:Request, res:Response) => {
         return res.status(500).send("Server error");
     }
 }
+
+export const getMovieById = async (req:Request, res:Response) => {
+    try {
+        const id = req.params.id;
+        const movie = await MovieModel.findById(id);
+
+        if(!movie) {
+            return res.status(404).json({Error: " O filme não existe."});
+        }
+
+        return res.status(200).send(movie);
+    } catch (error: any) {
+        Logger.error(`Error no sistema ${error.message}`);
+        return res.status(500).send("Server error");
+    }
+}
+
+export const getAllMovies = async (req:Request, res:Response) => {
+    try {
+        const movies = await MovieModel.find();
+
+        return res.status(200).send(movies);
+    } catch (error: any) {
+        Logger.error(`Error no sistema ${error.message}`);
+        return res.status(500).send("Server error");
+    }
+}
+
+export const removeMovie = async (req:Request, res:Response) => {
+    try {
+        const id = req.params.id;
+        const movie = await MovieModel.findById(id);
+
+        if(!movie) {
+            return res.status(404).json({Error: " O filme não existe."});
+        }
+
+        await movie.deleteOne();
+
+        return res.status(204).send();
+
+    } catch (error: any) {
+        Logger.error(`Error no sistema ${error.message}`);
+        return res.status(500).send("Server error");
+    }
+}
+
+export const updateMovie = async (req:Request, res:Response) => {
+    try {
+        const id = req.params.id;
+        const data = req.body;
+
+        const movie = await MovieModel.findById(id);
+
+        if(!movie) {
+            return res.status(404).json({Error: " O filme não existe."});
+        }
+
+        await MovieModel.updateOne({_id: id}, data);
+
+        return res.status(200).send(data);
+
+    } catch (error: any) {
+        Logger.error(`Error no sistema ${error.message}`);
+        return res.status(500).send("Server error");
+    }
+}
+
+
