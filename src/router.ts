00acd5fe9d0732d7909d03ea3bc1a5b97b717b29
Willian Costa <willian95@outlook.com>
2024-07-11 10:07:54 -0300

    feat: adição de rotas para endpoint de movie

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,8 @@
 import express ,{ Request, Response } from "express";
-import { createMovie } from "./controllers/MovieControllers";
+import { createMovie, getAllMovies, getMovieById, removeMovie, updateMovie } from "./controllers/MovieControllers";
+import { validate } from "./middleware/handleValidation";
+import { movieCreateValidation } from "./middleware/movieValidations";
+import { idParamValidation } from "./middleware/idValidation";
 
 const router = express.Router();
 
@@ -7,6 +10,14 @@ router.get("/teste", (req:Request, res:Response) => {
     res.status(200).send("API Funcionando!");
 });
 
-router.post("/movie", createMovie);
+router.post("/movie", movieCreateValidation(), validate, createMovie);
+
+router.get("/movie/:id", idParamValidation(), validate, getMovieById);
+
+router.get("/movie", getAllMovies);
+
+router.delete("/movie/:id", idParamValidation(), validate, removeMovie);
+
+router.patch("/movie/:id", idParamValidation(), movieCreateValidation(), validate, updateMovie);
 
 export default router;
